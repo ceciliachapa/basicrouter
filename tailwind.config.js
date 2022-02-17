d5c7babf611e4755c0035d0467a20136d3285336
ceciliachapa <cecilia.chapa91@gmail.com>
2022-02-16 22:28:04 -0600

    fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   mode: "jit",
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
